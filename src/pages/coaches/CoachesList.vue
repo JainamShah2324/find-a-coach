c6a32dab42871e0f1a4ce8fe6df8df3a680fd4b0
Jainam Shah <jainamtirthshah1998@gmail.com>
2021-03-11 17:55:31 +0530

    Coaches List and List Items components

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -8,9 +8,13 @@
             <router-link tag="button" :to="{ name: 'register-couch' }">Register as Coach</router-link>
         </div>
         <ul v-if="hasCoaches">
-            <li v-for="coach in filteredCourses" :key="coach.id">
-                {{ coach.firstName }}
-            </li>
+            <coach-item v-for="coach in filteredCoaches" :key="coach.id"
+                :id="coach.id"
+                :first-name="coach.firstName"
+                :last-name="coach.lastName"
+                :rate="coach.hourlyRate"
+                :areas="coach.areas"
+            ></coach-item>
         </ul>
         <h3 v-else>
             No coaches found.
@@ -19,16 +23,33 @@
 </template>
 
 <script>
+import CoachItem from '@/components/coaches/CoachItem';
+
 export default {
     name: "CoachesList",
+    components: {
+        CoachItem
+    },
     computed: {
-        filteredCourses() {
+        filteredCoaches() {
             return this.$store.getters['coaches/coaches'];
         },
         hasCoaches() {
-            console.log(this.$store.getters['coaches/hasCoaches']);
             return this.$store.getters['coaches/hasCoaches'];
         }
     }
 }
 </script>
+
+<style scoped>
+ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+}
+
+.controls {
+    display: flex;
+    justify-content: space-between;
+}
+</style>
